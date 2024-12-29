d4c306f3b6f2b004635c4f5606889a21d64983fc
Just Monika <Monika@ddlc.plus>
2024-12-29 23:33:46 +0800

    OOP化

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-
+#![feature(c_variadic)]
+#[macro_use]
+mod macros;
 use std::{ffi::CString, os::raw::c_void, ptr::null_mut};
-include!(concat!(env!("OUT_DIR"), "/bindgens.rs"));
+mod JNI;
+use JNI::{
+    JNI_CreateJavaVM, JNI_TRUE, JNI_VERSION_21, JNIEnv, JavaVM, JavaVMInitArgs, JavaVMOption,
+    jclass, jfieldID, jmethodID, jobject, jstring, va_list,
+};
+
+impl JNIEnv {
+    gen_jni_method!(NewStringUTF, jstring, chars: *const i8);
+    gen_jni_method!(FindClass,jclass,name: *const i8);
+    gen_jni_method!(CallVoidMethod,(),obj: jobject, methodID: jmethodID, args: va_list);
+    gen_jni_method!(GetMethodID,jmethodID,clazz: jclass,name: *const i8,sig: *const i8);
+    gen_jni_method!(GetStaticObjectField,jobject, clazz: jclass, fieldID: jfieldID);
+    gen_jni_method!(GetStaticFieldID,jfieldID,clazz: jclass,
+        name: *const i8,
+        sig: *const i8);
+}
+
+impl JavaVM {
+    gen_jni_method!(DestroyJavaVM, i32);
+}
 
 fn main() {
     let optionString = CString::new("").unwrap().into_raw();
@@ -18,37 +39,25 @@ fn main() {
         ignoreUnrecognized: JNI_TRUE as u8,
     };
     let mut jvm = null_mut::<JavaVM>();
+
     let mut jenv = null_mut::<JNIEnv>();
+
     unsafe {
         JNI_CreateJavaVM(
             &mut jvm,
             &mut jenv as *mut *mut JNIEnv as *mut *mut c_void,
             &mut vm_args as *mut JavaVMInitArgs as *mut c_void,
         );
-        let jenv_fn = jenv.as_ref().unwrap().functions.as_ref().unwrap();
-        let jvm_fn = jvm.as_ref().unwrap().functions.as_ref().unwrap();
-        let NewStringUTF = jenv_fn.NewStringUTF.unwrap();
-        let system_class = jenv_fn.FindClass.unwrap()(jenv, c("java/lang/System"));
-        let outField = jenv_fn.GetStaticFieldID.unwrap()(
-            jenv,
-            system_class,
-            c("out"),
-            c("Ljava/io/PrintStream;"),
-        );
-        let outObj = jenv_fn.GetStaticObjectField.unwrap()(jenv, system_class, outField);
-        let printStreamClass = jenv_fn.FindClass.unwrap()(jenv, c("java/io/PrintStream"));
-        let printlnMethod = jenv_fn.GetMethodID.unwrap()(
-            jenv,
-            printStreamClass,
-            c("println"),
-            c("(Ljava/lang/String;)V"),
-        );
-        let msg = NewStringUTF(jenv, c("Hello World"));
-        jenv_fn.CallVoidMethod.unwrap()(jenv, outObj, printlnMethod, msg);
-        jvm_fn.DestroyJavaVM.unwrap()(jvm);
-    }
-}
+        let system_class = (*jenv).FindClass(c!("java/lang/System"));
+        let outField =
+            (*jenv).GetStaticFieldID(system_class, c!("out"), c!("Ljava/io/PrintStream;"));
+        let outObj = (*jenv).GetStaticObjectField(system_class, outField);
+        let printStreamClass = (*jenv).FindClass(c!("java/io/PrintStream"));
+        let printlnMethod =
+            (*jenv).GetMethodID(printStreamClass, c!("println"), c!("(Ljava/lang/String;)V"));
+        let msg = (*jenv).NewStringUTF(c!("Hello You"));
+        (*jenv).CallVoidMethod(outObj, printlnMethod, msg as va_list);
 
-fn c(s: &str) -> *const i8 {
-    CString::new(s).unwrap().into_raw()
+        (*jvm).DestroyJavaVM();
+    }
 }
