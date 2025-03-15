bb291d5e229a6b0d7e26fac9f868546d64f8cc63
Monika <Monika>
2025-03-15 20:48:54 +0800

    remove useless macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,28 +1,3 @@
-macro_rules! gen_jni_method {
-    ($method_name:ident, $return_type:ty) => {
-        fn $method_name(&mut self) -> $return_type {
-            unsafe {
-                self.functions
-                    .as_ref()
-                    .unwrap()
-                    .$method_name
-                    .unwrap()(self)
-            }
-        }
-    };
-    ($method_name:ident, $return_type:ty, $($param_name:ident: $param_type:ty),*) => {
-        fn $method_name(&mut self, $($param_name: $param_type),*) -> $return_type {
-            unsafe {
-                self.functions
-                    .as_ref()
-                    .unwrap()
-                    .$method_name
-                    .unwrap()(self, $($param_name),*)
-            }
-        }
-    };
-}
-
 macro_rules! c {
     ($s:expr) => {
         CString::new($s).unwrap().into_raw()
