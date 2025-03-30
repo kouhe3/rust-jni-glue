d61d94d33e30b273ba5eb87aa0809bc9774f051f
kouhe3 <25522053+kouhe3@users.noreply.github.com>
2025-03-30 10:54:30 +0000

    linux version build.rs

--- build.rs
+++ build.rs
@@ -2,13 +2,24 @@ use std::env;
 
 fn main() {
     let java_home = env::var("JAVA_HOME").expect("JAVA_HOME not set");
-    println!("cargo:rustc-link-search={}lib", java_home);
+    let platform = if cfg!(target_os = "windows"){
+        "win32"
+    }else if cfg!(target_os = "linux") {
+        "linux"
+    }else{
+        panic!("Not support yet");
+    };
+    if platform == "linux"{
+        println!("cargo:rustc-link-search=native={}/lib/server", java_home);
+    }else{
+        println!("cargo:rustc-link-search=native={}/lib", java_home);
+    }
     println!("cargo:rustc-link-lib=jvm");
 
     let bindgens = bindgen::Builder::default()
         .header("wrap.h")
-        .clang_arg(format!("-I{java_home}include"))
-        .clang_arg(format!("-I{java_home}include/win32"))
+        .clang_arg(format!("-I{java_home}/include"))
+        .clang_arg(format!("-I{java_home}/include/{platform}"))
         .clang_arg("-x")
         .clang_arg("c++")
         .generate()
