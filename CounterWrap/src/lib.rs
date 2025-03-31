3f110088a5627b9d9b0cf31c7694ec929a081226
Monika <Monika>
2025-03-31 19:16:57 +0800

    包装 Counter

---
+++ CounterWrap/src/lib.rs
@@ -0,0 +1,34 @@
+macro_rules! c {
+    ($s:expr) => {
+        ::std::ffi::CString::new($s).unwrap().into_raw()
+    };
+}
+
+use JNIWrap::JNI::{jobject, jvalue, JNIEnv,jint};
+
+pub struct Counter{
+    pub obj: jobject,
+    pub env: *mut JNIEnv,
+}
+impl Counter{
+    pub unsafe fn add(jenv: *mut JNIEnv, a: i32, b:i32) -> Option<i32>{
+        unsafe{
+            let class = (*jenv).FindClass(c!("Counter"))?;
+            let method = (*jenv).GetStaticMethodID(class, c!("add"), c!("(II)I"))?;
+            let jvalue_list = [jvalue::jint(a)?, jvalue::jint(b)?];
+            (*jenv).CallStaticIntMethodA(class, method, jvalue_list.as_ptr()).map(|x| x as i32)
+            
+        }
+        
+    }
+    pub unsafe fn main(jenv: *mut JNIEnv, args: &[&str]) -> Option<()>{
+        unsafe {
+            let class = (*jenv).FindClass(c!("Counter"))?;
+            let method = (*jenv).GetStaticMethodID(class, c!("main"), c!("([Ljava/lang/String;)V"))?;
+            let jvalue_list: Vec<jvalue> = args.iter().map(|s| unsafe{jvalue::str(jenv, s)}).filter_map(|f| f).collect();
+            (*jenv).CallStaticVoidMethodA(class, method, jvalue_list.as_ptr())
+
+        }
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use PersonWrap::Person;
-
+use CounterWrap::Counter;
 use JNIWrap::CreateJavaWrapper;
 use JNIWrap::JNI::{JNI_VERSION_21, JavaVMInitArgs, JavaVMOption};
-fn main() {
-    let mut vmoptions = JavaVMOption::new(r"-Djava.class.path=PersonWrap/java");
+fn main() -> std::io::Result<()> {
+    let mut vmoptions = JavaVMOption::new(r"-Djava.class.path=CounterWrap/java;PersonWrap/java");
     let vmargs = JavaVMInitArgs::new(JNI_VERSION_21, 1, &mut vmoptions, true);
     let (jvm, jenv) = CreateJavaWrapper(vmargs);
     let mut zhangsan = unsafe { Person::new(jenv, "zhangsan", 18) }.unwrap();
     unsafe {
+        Counter::main(jenv, &["x"]);
+        println!("5+8={}", Counter::add(jenv, 5, 8).unwrap());
         zhangsan.introduce().unwrap();
         (*jvm).DestroyJavaVM();
     }
     println!("Finish");
+    Ok(())
 }
