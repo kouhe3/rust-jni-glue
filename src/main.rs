34891185576faa25f65ad399ff948992a8a1b878
Monika <Monika>
2025-03-31 16:25:09 +0800

    change classpath

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use PersonWrap::Person;
 use JNIWrap::CreateJavaWrapper;
 use JNIWrap::JNI::{JNI_VERSION_21, JavaVMInitArgs, JavaVMOption};
 fn main() {
-    let mut vmoptions = JavaVMOption::new(r"-Djava.class.path=.");
+    let mut vmoptions = JavaVMOption::new(r"-Djava.class.path=PersonWrap/java");
     let vmargs = JavaVMInitArgs::new(JNI_VERSION_21, 1, &mut vmoptions, true);
     let (mut jvm, mut jenv) = CreateJavaWrapper(vmargs);
     let mut zhangsan = Person::new(jenv, "zhangsan", 18).unwrap();
