3caadd59cd9241894ff7a3b653d3142c598b42f1
Monika <Monika>
2025-04-05 22:41:56 +0800

    添加注释

--- JNIWrap/JNIWrap.rs
+++ JNIWrap/JNIWrap.rs
@@ -1,35 +1,81 @@
+// 允许使用非蛇形命名法，因为 JNI 相关的函数和类型通常遵循特定的命名规范
 #![allow(non_snake_case)]
+// 引入 jnimacro 库中的 jni_method 过程宏，用于生成 JNI 方法调用的代码
 use jnimacro::jni_method;
+// 公开 JNI 模块，该模块包含 JNI 相关的类型和函数定义
 pub mod JNI;
 
+// 引入 JNI 模块中的各种类型和函数
 use JNI::{
     JNI_CreateJavaVM, JNIEnv, JavaVM, JavaVMInitArgs, JavaVMOption, jclass, jfieldID, jint, jlong,
     jmethodID, jobject, jstring, jvalue,
 };
+// 引入标准库中的相关类型和函数
 use std::{ffi::CString, os::raw::c_void, ptr::null_mut};
 
+/// 创建 Java 虚拟机和 JNI 环境的包装函数
+///
+/// 该函数接受 Java 虚拟机的初始化参数，并尝试创建 Java 虚拟机和 JNI 环境。
+///
+/// # 参数
+/// - `vm_args`: Java 虚拟机的初始化参数
+///
+/// # 返回值
+/// 一个元组，包含指向 Java 虚拟机的指针和指向 JNI 环境的指针
 pub fn CreateJavaWrapper(mut vm_args: JavaVMInitArgs) -> (*mut JavaVM, *mut JNIEnv) {
+    // 初始化指向 Java 虚拟机的指针为空
     let mut pjvm = null_mut::<JavaVM>();
+    // 初始化指向 JNI 环境的指针为空
     let mut pjenv = null_mut::<JNIEnv>();
+    // 由于 JNI 函数是不安全的，需要使用 unsafe 块
     unsafe {
+        // 调用 JNI_CreateJavaVM 函数创建 Java 虚拟机和 JNI 环境
         JNI_CreateJavaVM(
             &mut pjvm,
             &mut pjenv as *mut *mut JNIEnv as *mut *mut c_void,
             &mut vm_args as *mut JavaVMInitArgs as *mut c_void,
         );
 
+        // 返回指向 Java 虚拟机和 JNI 环境的指针
         (pjvm, pjenv)
     }
 }
+
+/// JavaVMOption 结构体的实现
 impl JavaVMOption {
+    /// 创建一个新的 JavaVMOption 实例
+    ///
+    /// 该函数接受一个字符串参数，并将其转换为 C 字符串，用于设置 Java 虚拟机的选项。
+    ///
+    /// # 参数
+    /// - `s`: 用于设置 Java 虚拟机选项的字符串
+    ///
+    /// # 返回值
+    /// 一个新的 JavaVMOption 实例
     pub fn new(s: &str) -> Self {
         JavaVMOption {
+            // 额外信息指针初始化为空
             extraInfo: std::ptr::null_mut(),
+            // 将输入的字符串转换为 C 字符串，并获取其指针
             optionString: CString::new(s).unwrap().into_raw(),
         }
     }
 }
+
+/// JavaVMInitArgs 结构体的实现
 impl JavaVMInitArgs {
+    /// 创建一个新的 JavaVMInitArgs 实例
+    ///
+    /// 该函数接受 Java 虚拟机的版本、选项数量、选项数组和是否忽略未识别选项的标志，并返回一个新的 JavaVMInitArgs 实例。
+    ///
+    /// # 参数
+    /// - `version`: Java 虚拟机的版本
+    /// - `nOptions`: 选项的数量
+    /// - `options`: 选项数组的可变引用
+    /// - `ignoreUnrecognized`: 是否忽略未识别的选项
+    ///
+    /// # 返回值
+    /// 一个新的 JavaVMInitArgs 实例
     pub fn new(
         version: u32,
         nOptions: i32,
@@ -37,14 +83,19 @@ impl JavaVMInitArgs {
         ignoreUnrecognized: bool,
     ) -> Self {
         JavaVMInitArgs {
+            // 将版本转换为 i32 类型
             version: version as i32,
+            // 选项的数量
             nOptions,
+            // 选项数组的引用
             options,
+            // 将布尔值转换为 u8 类型
             ignoreUnrecognized: ignoreUnrecognized as u8,
         }
     }
 }
 
+/// JNIEnv 结构体的实现，定义 JNI 环境的方法
 impl JNIEnv {
     jni_method!(NewObjectA: (jclass, jmethodID, *const jvalue) -> Option<jobject>  );
     jni_method!(FindClass: (*const ::std::os::raw::c_char) -> Option<jclass>);
@@ -58,10 +109,12 @@ impl JNIEnv {
     jni_method!(GetObjectClass: (jobject) -> Option<jclass>);
 }
 
+/// JavaVM 结构体的实现，定义 Java 虚拟机的方法
 impl JavaVM {
     jni_method!(DestroyJavaVM: () -> Option<i32>);
 }
 
+/// jvalue 结构体的实现，定义创建不同类型 jvalue 的方法
 impl jvalue {
     /// 创建一个包含 Java 字符串的 `jvalue`。
     ///
@@ -72,7 +125,11 @@ impl jvalue {
     /// - 返回的 `jstring` 是一个本地引用，调用者需要确保在调用后正确管理其生命周期。
     pub unsafe fn str(pjenv: *mut JNIEnv, s: &str) -> jvalue {
         jvalue {
-            l: unsafe { (*pjenv).NewStringUTF(CString::new(s).unwrap().as_ptr()).unwrap() as jobject },
+            l: unsafe {
+                (*pjenv)
+                    .NewStringUTF(CString::new(s).unwrap().as_ptr())
+                    .unwrap() as jobject
+            },
         }
     }
 
--- jnimacro/lib.rs
+++ jnimacro/lib.rs
@@ -1,23 +1,41 @@
+// 引入必要的包
 use proc_macro::TokenStream as Tok1;
 use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote};
 use syn::{Expr, Ident, LitStr, ParenthesizedGenericArguments, Token, parse_macro_input};
 use syn_derive::Parse;
 
+/// 解析函数结构的结构体
+/// 用于解析宏输入的函数定义，包含函数名、参数和返回类型等信息
 #[derive(Parse)]
 struct FnStruct {
+    // 函数名
     fn_name: Ident,
+    // 分隔符
     _col: Token![:],
+    // 函数参数和返回类型
     args: ParenthesizedGenericArguments,
 }
+
 impl FnStruct {
+    /// 展开函数定义为实际的 Rust 代码
+    ///
+    /// # 返回值
+    /// 展开后的 TokenStream
     pub fn expand(&self) -> TokenStream {
+        // 获取函数名
         let name = &self.fn_name;
+        // 获取函数返回类型
         let ret = &self.args.output;
+        // 生成参数标识符的基础名称
         let ident = Ident::new("n", Span::call_site());
+        // 迭代函数的输入参数类型
         let types = self.args.inputs.iter();
+        // 生成参数标识符
         let idents = (0..types.len()).map(|i| format_ident!("{}{}", ident, i));
+        // 克隆参数标识符，用于函数调用
         let idents_clone = idents.clone();
+        // 使用 quote 宏生成实际的 Rust 代码
         quote! {
             pub unsafe fn #name(&mut self,#(#idents: #types),*) #ret{
                 unsafe {
@@ -29,54 +47,113 @@ impl FnStruct {
         }
     }
 }
+
+/// 处理 jni_method 宏的过程宏
+///
+/// # 参数
+/// - `input`: 宏输入的 TokenStream
+///
+/// # 返回值
+/// 处理后的 TokenStream
 #[proc_macro]
 pub fn jni_method(input: Tok1) -> Tok1 {
+    // 解析输入为 FnStruct 结构体
     let x: FnStruct = parse_macro_input!(input as FnStruct);
+    // 展开结构体为实际的 Rust 代码
     let x = x.expand();
+    // 将展开后的代码转换为 TokenStream 并返回
     proc_macro::TokenStream::from(x)
 }
+
+/// 用于解析构造函数体输入的结构体
+/// 包含 JNI 环境指针、参数、类名和描述符等信息
 #[derive(Parse)]
 struct ConstructorBodyInput {
+    // JNI 环境指针
     jenv: Expr,
+    // 分隔符
     _comma1: Token![,],
+    // 参数
     args: Expr,
+    // 分隔符
     _comma2: Token![,],
+    // 类名
     class_name: LitStr,
+    // 分隔符
     _comma3: Token![,],
+    // 描述符
     descriptor: LitStr,
 }
 
+/// 用于解析实例方法体输入的结构体
+/// 包含 JNI 环境指针、参数、对象实例、方法名和描述符等信息
 #[derive(Parse)]
 struct MethodBodyInput {
+    // JNI 环境指针
     jenv: Expr,
+    // 分隔符
     _comma1: Token![,],
+    // 参数
     args: Expr,
+    // 分隔符
     _comma2: Token![,],
+    // 对象实例
     obj: Expr,
+    // 分隔符
     _comma4: Token![,],
+    // 方法名
     method_name: LitStr,
+    // 分隔符
     _comma: Token![,],
+    // 描述符
     descriptor: LitStr,
 }
+
+/// 用于解析静态方法体输入的结构体
+/// 包含 JNI 环境指针、参数、类名、方法名和描述符等信息
 #[derive(Parse)]
 struct StaticMethodBodyInput {
+    // JNI 环境指针
     jenv: Expr,
+    // 分隔符
     _comma1: Token![,],
+    // 参数
     args: Expr,
+    // 分隔符
     _comma2: Token![,],
+    // 类名
     class_name: LitStr,
+    // 分隔符
     _comma3: Token![,],
+    // 方法名
     method_name: LitStr,
+    // 分隔符
     _comma4: Token![,],
+    // 描述符
     descriptor: LitStr,
 }
+
+/// 处理 jni_constructor_body 宏的过程宏
+/// 用于生成调用 JNI 构造函数的代码
+///
+/// # 参数
+/// - `input`: 宏输入的 TokenStream
+///
+/// # 返回值
+/// 处理后的 TokenStream
 #[proc_macro]
 pub fn jni_constructor_body(input: Tok1) -> Tok1 {
+    // 解析输入为 ConstructorBodyInput 结构体
     let input = parse_macro_input!(input as ConstructorBodyInput);
+    // 获取 JNI 环境指针
     let jenv = input.jenv;
+    // 获取参数
     let args = input.args;
+    // 获取类名
     let class_name_str = input.class_name;
+    // 获取描述符
     let descriptor_str = input.descriptor;
+    // 使用 quote 宏生成调用 JNI 构造函数的代码
     let x = quote! {
         unsafe{
             let jenv_ptr = #jenv;
@@ -90,19 +167,37 @@ pub fn jni_constructor_body(input: Tok1) -> Tok1 {
             Some(Self(obj))
         }
     };
+    // 将生成的代码转换为 TokenStream 并返回
     Tok1::from(x)
 }
 
+/// 处理 jni_method_body 宏的过程宏
+/// 用于生成调用 JNI 实例方法的代码
+///
+/// # 参数
+/// - `input`: 宏输入的 TokenStream
+///
+/// # 返回值
+/// 处理后的 TokenStream
 #[proc_macro]
 pub fn jni_method_body(input: Tok1) -> Tok1 {
+    // 解析输入为 MethodBodyInput 结构体
     let input = parse_macro_input!(input as MethodBodyInput);
+    // 获取对象实例
     let obj = input.obj;
+    // 获取 JNI 环境指针
     let jenv = input.jenv;
+    // 获取参数
     let args = input.args;
+    // 获取方法名
     let method_name = input.method_name;
+    // 获取描述符
     let descriptor = input.descriptor;
+    // 根据描述符确定返回类型
     let return_type = return_type(descriptor.value());
+    // 生成调用方法的标识符
     let identname = format_ident!("Call{}MethodA", return_type);
+    // 使用 quote 宏生成调用 JNI 实例方法的代码
     let x = quote! {
         unsafe {
             let method_name_cstr = ::std::ffi::CString::new(#method_name).expect("Invalid method name string literal");
@@ -116,19 +211,37 @@ pub fn jni_method_body(input: Tok1) -> Tok1 {
             Some(r)
         }
     };
+    // 将生成的代码转换为 TokenStream 并返回
     Tok1::from(x)
 }
 
+/// 处理 jni_static_method_body 宏的过程宏
+/// 用于生成调用 JNI 静态方法的代码
+///
+/// # 参数
+/// - `input`: 宏输入的 TokenStream
+///
+/// # 返回值
+/// 处理后的 TokenStream
 #[proc_macro]
 pub fn jni_static_method_body(input: Tok1) -> Tok1 {
+    // 解析输入为 StaticMethodBodyInput 结构体
     let input = parse_macro_input!(input as StaticMethodBodyInput);
+    // 获取 JNI 环境指针
     let jenv = input.jenv;
+    // 获取参数
     let args = input.args;
+    // 获取类名
     let class_name = input.class_name;
+    // 获取方法名
     let method_name = input.method_name;
+    // 获取描述符
     let descriptor = input.descriptor;
+    // 根据描述符确定返回类型
     let return_type = return_type(descriptor.value());
+    // 生成调用静态方法的标识符
     let identname = format_ident!("CallStatic{}MethodA", return_type);
+    // 使用 quote 宏生成调用 JNI 静态方法的代码
     let x = quote! {
         unsafe {
             let class_name_cstr = ::std::ffi::CString::new(#class_name).expect("Invalid class name string literal");
@@ -143,13 +256,25 @@ pub fn jni_static_method_body(input: Tok1) -> Tok1 {
             Some(r)
         }
     };
+    // 将生成的代码转换为 TokenStream 并返回
     Tok1::from(x)
 }
 
+/// 根据描述符确定返回类型
+///
+/// # 参数
+/// - `descriptor`: 方法描述符
+///
+/// # 返回值
+/// 返回类型的字符串表示
 fn return_type(descriptor: String) -> &'static str {
+    // 获取描述符的最后一个字符
     match descriptor.chars().last() {
+        // 如果是 'I'，表示返回类型为整数
         Some('I') => "Int",
+        // 如果是 'V'，表示返回类型为 void
         Some('V') => "Void",
+        // 其他情况抛出异常
         _ => panic!("Invalid return type"),
     }
 }
